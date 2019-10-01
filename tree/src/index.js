75dca05aaaed65c24ace656a71548f1f0b108c0f
Nate <nate@Strides-MacBook-Pro-3.local>
2019-10-01 18:00:23 -0400

    add react-sortable-tree

--- tree/src/index.js
+++ tree/src/index.js
@@ -1,7 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
+import SortableTree from "react-sortable-tree";
+import './styles.css';
+import 'react-sortable-tree/style.css';
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('root')
-);
+const Employees = () => {
+  const treeData = [
+    { title: "Chicken", expanded: true, children: [{ title: "Egg" }] }
+  ];
+  return (
+    <div style={{ height: 500 }}>
+      <SortableTree treeData={treeData} />
+    </div>
+  );
+}
+
+render(<Employees />, document.getElementById("root"));
--- tree/webpack.config.js
+++ tree/webpack.config.js
@@ -15,6 +15,9 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader"
+      }, {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
