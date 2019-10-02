c318a1caab8c5f91c655c9faf929b533a1fca822
Nathan Jones <lemaitre86@gmail.com>
2019-10-02 11:37:31 -0400

    display stride employees as tree

--- bamboo/index.js
+++ bamboo/index.js
@@ -7,6 +7,12 @@ const APP_VERSION = "1.0.0"
 const appInfo = { appName: APP_NAME, version: APP_VERSION };
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', (req, res, next) => {
   res.send(appInfo);
   return next();
--- tree/src/index.js
+++ tree/src/index.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
 import SortableTree from "react-sortable-tree";
 import './styles.css';
 import 'react-sortable-tree/style.css';
 
 const Employees = () => {
-  const treeData = [
-    { title: "Chicken", expanded: true, children: [{ title: "Egg" }] }
-  ];
+  const [treeData, setTreeData] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:4000/people`)
+      .then(response => response.json())
+      .then(data => {
+        const mappedData = data.map(({id, firstName, lastName}) => (
+          {
+            key: id,
+            title: `${firstName} ${lastName}`,
+            expanded: false,
+            children: []
+          }
+        ));
+        setTreeData(mappedData)
+      });
+  });
+  const onMoveNode = ({
+    treeData,
+    node,
+    nextParentNode,
+    prevPath,
+    prevTreeIndex,
+    nextPath,
+    nextTreeIndex
+  }) => {
+    console.log(`Assign sponsee ${node.title} to sponser ${nextParentNode.title}`);
+  };
+  const onChange = () => null
   return (
-    <div style={{ height: 500 }}>
-      <SortableTree treeData={treeData} />
+    <div style={{ height: '100%' }}>
+      <SortableTree treeData={treeData} onMoveNode={onMoveNode} onChange={onChange}/>
     </div>
   );
 }
